c85bb154cf179ee4f9bb91055de4434de1f45417
princepal9120 <prince@bik.ai>
2025-08-03 02:35:33 +0530

    Removed broken submodule

---
+++ structs/src/main.rs
@@ -0,0 +1,19 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    signincount: u64,
+
+    
+}
+fn main() {
+    let user: User= User{
+        email: String::from("princepal@gmail.com"),
+        username: String::from("prince"),
+        active: true,
+        signincount: 4,
+
+    };
+
+    println!("user name is {} ", user.username);
+}
