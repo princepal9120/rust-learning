77d88c9e63a41712346e4d6da054c744932627da
princepal9120 <prince@bik.ai>
2025-08-04 23:33:10 +0530

    Add initial project structure and example code for Rust learning

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -4,16 +4,23 @@ struct User {
     email: String,
     signincount: u64,
 
-    
+
 }
 fn main() {
-    let user: User= User{
-        email: String::from("princepal@gmail.com"),
-        username: String::from("prince"),
-        active: true,
-        signincount: 4,
+    // let user: User= User{
+    //     email: String::from("princepal@gmail.com"),
+    //     username: String::from("prince"),
+    //     active: true,
+    //     signincount: 4,
 
-    };
+    // };
 
-    println!("user name is {} ", user.username);
+    // println!("user name is {} ", user.username);
+    calculat_area();
+}
+
+// program to understadn the rust
+
+fn calculat_area() -> str{
+return "hello";
 }
