71e0759a03f64a68b9fd4c69751d2bd193f8cd2d
Prince <prince@Princes-MacBook-Pro.local>
2025-08-16 03:30:44 +0530

    complte struct ownership

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -6,21 +6,58 @@ struct User {
 
 
 }
+// fn main() {
+//     // let user: User= User{
+//     //     email: String::from("princepal@gmail.com"),
+//     //     username: String::from("prince"),
+//     //     active: true,
+//     //     signincount: 4,
+
+//     // };
+
+//     // println!("user name is {} ", user.username);
+
+// }
+
+
+
+//more on structs
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+// strunct on ownership
+
+impl Rectangle {
+    // method to calculate area
+    fn new(width: u32, height: u32) -> Self {
+        Self { width, height }
+    }
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    // let user: User= User{
-    //     email: String::from("princepal@gmail.com"),
-    //     username: String::from("prince"),
-    //     active: true,
-    //     signincount: 4,
-
+    // let rect1 = Rectangle {
+    //     width: 30,
+    //     height: 50,
     // };
+    // let rect2: Rectangle = Rectangle {
+    //     width: 10,
+    //     height: 40,
+    // };
+    let rect3 = Rectangle::new(20, 60);
 
-    // println!("user name is {} ", user.username);
-    calculat_area();
+    println!("The area of the rectangle1 is {} square pixels.", area(&Rectangle::new(30, 50)));
+    println!("The area of the rectangle2 is {} square pixels.", Rectangle::new(10, 40).area());
+    println!("The area of the rectangle3 is {} square pixels.", rect3.area());
+
 }
 
-// program to understadn the rust
 
-fn calculat_area() -> str{
-return "hello";
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
+
